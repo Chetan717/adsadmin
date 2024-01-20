81eb7d50b1c3841939b5e1b45d90b89d40e75de2
Chetan717 <chaitanyachaudhari717@gmail.com>
2024-01-20 09:58:33 +0530

    20 jan changes Done

--- src/pages/Template/Comp/EditTemp.tsx
+++ src/pages/Template/Comp/EditTemp.tsx
@@ -106,8 +106,8 @@ export default function EditTemplate({
           >
             {/* Options for bannerId */}
             {TemplateType?.map((option: any) => (
-              <option key={option} value={option}>
-                {option}
+              <option key={option?.value} value={option?.value}>
+                {option?.name}
               </option>
             ))}
           </select>
